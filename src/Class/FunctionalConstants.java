e0383315258a4c43e0a5e8760c6f63a335504dda
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-02-10 22:52:50 +0000

    Destinations implementation part 1

---
+++ src/Class/FunctionalConstants.java
@@ -0,0 +1,15 @@
+package Class;
+
+public class FunctionalConstants {
+	/** Year constants **/
+	public static final String YEAR_2014 = "2014";
+	public static final String YEAR_2015 = "2015";
+	public static final String YEAR_BOTH = "BOTH";
+	
+	/** Destination constants **/
+	public static final String DEST_VALID = "DEST_VALID";
+	public static final String DEST_VISITED = "DEST_VISITED";
+	
+	/** Line return **/
+	public static final String NEW_LINE = "\n";
+}
