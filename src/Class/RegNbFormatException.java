3d3b237ff245e61a71eb9adbe4eb8071543aac86
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-02-23 15:31:59 +0000

    reg nb format exception

---
+++ src/Class/RegNbFormatException.java
@@ -0,0 +1,10 @@
+package Class;
+
+public class RegNbFormatException extends Exception {
+	/** Serial */
+	private static final long serialVersionUID = 5538631979342018432L;
+
+	public RegNbFormatException(String RegNb){
+		super("Registration number not conform: " + RegNb);
+	}
+}
